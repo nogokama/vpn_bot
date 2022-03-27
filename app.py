412a9b1edd05b9060e2d87656a23539fe438dbe8
Artem Makogon <artem.makogon.nsk@gmail.com>
2022-03-27 14:12:01 +0300

    gather bot folder

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from aiogram import executor
 
-from loader import dp
-import middlewares, filters, handlers
-from utils.notify_admins import on_startup_notify
-from utils.set_bot_commands import set_default_commands
+from bot.loader import dp
+import bot.middlewares, bot.filters, bot.handlers
+from bot.utils.notify_admins import on_startup_notify
+from bot.utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
